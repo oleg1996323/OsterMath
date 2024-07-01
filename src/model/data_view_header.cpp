255b969bb4341ceab44e0486aa657ef9f4e7f352
oster <oleg.ternikov@gmail.com>
2024-07-01 19:03:02 +0300

    Tested recursive subelements of arrays for header

--- src/model/data_view_header.cpp
+++ src/model/data_view_header.cpp
@@ -2,104 +2,57 @@
 #include "model/varlistmodel.h"
 #include "types.h"
 #include "arithmetic_functions.h"
+#include <QStyle>
+#include <memory>
 
 namespace model {
+
+struct STRUCT_SECTION{
+    STRUCT_SECTION(STRUCT_SECTION* parent = nullptr):parent(parent){}
+
+    void update_max_size_depth(){
+        if(parent_ && parent_->max_size_depth<=max_size_depth)
+            parent_->max_size_depth=max_size_depth+1;
+    }
+
+    std::vector<STRUCT_SECTION> childs_;
+    STRUCT_SECTION* parent_;
+    size_t size_=0;
+    size_t max_size_depth = 0;
+};
+
+void define_section_subelements(STRUCT_SECTION& section_sizes, ArrayNode* array){
+    if(array->has_childs()){
+        section_sizes.size_=array->childs().size();
+        for(std::shared_ptr<Node> child:*array){
+            if(child->is_array()){
+                section_sizes.childs_.push_back(STRUCT_SECTION(&section_sizes));
+                define_section_subelements(section_sizes.childs_.back(), reinterpret_cast<ArrayNode*>(child->execute().get<Node*>()));
+                section_sizes.childs_.back().update_max_size_depth();
+            }
+        }
+    }
+}
 
 void DataViewHeader::paintSection(QPainter *painter, const QRect &rect, int logicalIndex) const{
     const Variables *model =
             qobject_cast<Variables *>(this->model());
 
-    std::vector<size_t> sz_depth_measure;
-    if(var_->is_array())
-        functions::auxiliary::init_sz_depth_measure(sz_depth_measure,reinterpret_cast<ArrayNode*>(var_->node()->child(0).get()));
+    STRUCT_SECTION section_sizes;
+    if(var_->is_array() && var_->node()->has_childs())
+        define_section_subelements(section_sizes,reinterpret_cast<ArrayNode*>(var_->node().get()));
+
+
 
     const size_t depth = sz_depth_measure.size();
     for (int i = 0; i < depth; ++i) {
-        QModelIndex cellIndex = (orientation() == Qt::Horizontal)
-                ? model->index(i, logicalIndex)
-                : model->index(logicalIndex, i);
-        QSize cellSize = cellIndex.data(Qt::SizeHintRole).toSize();
+        QModelIndex cellIndex = model->index(i, logicalIndex);
+        QSize cellSize = {400,20};
         QRect sectionRect(rect);
 
-        // set position of the cell
-        if (orientation() == Qt::Horizontal)
-            sectionRect.setTop(
-                        rowSpanSize(logicalIndex, 0, i)); // distance from 0 to i-1 rows
-        else
-            sectionRect.setLeft(columnSpanSize(logicalIndex, 0, i));
+        sectionRect.setTop(rowSpanSize(logicalIndex, 0, i)); // distance from 0 to i-1 rows
 
         sectionRect.setSize(cellSize);
-
-        // check up span column or row
-        QModelIndex colSpanIdx = columnSpanIndex(cellIndex);
-        QModelIndex rowSpanIdx = rowSpanIndex(cellIndex);
-        if (colSpanIdx.isValid()) {
-            int colSpanFrom = colSpanIdx.column();
-            int colSpanCnt =
-                    colSpanIdx.data(GridTableHeaderModel::ColumnSpanRole).toInt();
-            int colSpanTo = colSpanFrom + colSpanCnt - 1;
-            int colSpan = columnSpanSize(cellIndex.row(), colSpanFrom, colSpanCnt);
-            if (orientation() == Qt::Horizontal)
-                sectionRect.setLeft(sectionViewportPosition(colSpanFrom));
-            else {
-                sectionRect.setLeft(columnSpanSize(logicalIndex, 0, colSpanFrom));
-                i = colSpanTo;
-            }
-
-            sectionRect.setWidth(colSpan);
-
-            // check up  if the column span index has row span
-            QVariant subRowSpanData =
-                    colSpanIdx.data(GridTableHeaderModel::RowSpanRole);
-            if (subRowSpanData.isValid()) {
-                int subRowSpanFrom = colSpanIdx.row();
-                int subRowSpanCnt = subRowSpanData.toInt();
-                int subRowSpanTo = subRowSpanFrom + subRowSpanCnt - 1;
-                int subRowSpan =
-                        rowSpanSize(colSpanFrom, subRowSpanFrom, subRowSpanCnt);
-                if (orientation() == Qt::Vertical)
-                    sectionRect.setTop(sectionViewportPosition(subRowSpanFrom));
-                else {
-                    sectionRect.setTop(rowSpanSize(colSpanFrom, 0, subRowSpanFrom));
-                    i = subRowSpanTo;
-                }
-                sectionRect.setHeight(subRowSpan);
-            }
-            cellIndex = colSpanIdx;
-        }
-        if (rowSpanIdx.isValid()) {
-            int rowSpanFrom = rowSpanIdx.row();
-            int rowSpanCnt =
-                    rowSpanIdx.data(GridTableHeaderModel::RowSpanRole).toInt();
-            int rowSpanTo = rowSpanFrom + rowSpanCnt - 1;
-            int rowSpan = rowSpanSize(cellIndex.column(), rowSpanFrom, rowSpanCnt);
-            if (orientation() == Qt::Vertical)
-                sectionRect.setTop(sectionViewportPosition(rowSpanFrom));
-            else {
-                sectionRect.setTop(rowSpanSize(logicalIndex, 0, rowSpanFrom));
-                i = rowSpanTo;
-            }
-            sectionRect.setHeight(rowSpan);
-
-            // check up if the row span index has column span
-            QVariant subColSpanData =
-                    rowSpanIdx.data(GridTableHeaderModel::ColumnSpanRole);
-            if (subColSpanData.isValid()) {
-                int subColSpanFrom = rowSpanIdx.column();
-                int subColSpanCnt = subColSpanData.toInt();
-                int subColSpanTo = subColSpanFrom + subColSpanCnt - 1;
-                int subColSpan =
-                        columnSpanSize(rowSpanFrom, subColSpanFrom, subColSpanCnt);
-                if (orientation() == Qt::Horizontal)
-                    sectionRect.setLeft(sectionViewportPosition(subColSpanFrom));
-                else {
-                    sectionRect.setLeft(columnSpanSize(rowSpanFrom, 0, subColSpanFrom));
-                    i = subColSpanTo;
-                }
-                sectionRect.setWidth(subColSpan);
-            }
-            cellIndex = rowSpanIdx;
-        }
 
         // draw section with style
         QStyleOptionHeader opt;
